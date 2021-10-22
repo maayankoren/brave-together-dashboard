c706d911e03c3c6b1980203e35cfa44762470a39
ABC <dvivonbirati@gmail.com>
2021-10-22 15:59:30 +0300

    for mongo connection-->need to change

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from pymongo import MongoClient
+import pymongo
 app = Flask(__name__)
 
 app.config.from_object("config")
@@ -7,7 +8,54 @@ app.config.from_object("config")
 
 @app.route('/')
 def index():
-   return 'Hello Story'
+
+    CONNECTION_STRING = "mongodb+srv://admin:9WxI96T8baPlnBRW@cluster0.0qv9t.mongodb.net/brave_together?retryWrites=true&w=majority"
+    client = MongoClient(CONNECTION_STRING)
+    db = client['brave_together']
+    collection_name = db["story_tamplate"]
+    item = {
+    "storyUpload":{
+        "mainHeader":{
+            "content":"welcome",
+            "color":"white",
+            "fontSize":"14vh"
+        }
+        ,
+        "form":[
+            {
+                "label":"כותרת",
+                "tag":"input",
+                "type":"text"
+            },{
+                "label":"תוכן",
+                "tag":"textarea",
+                "rows":"4",
+                "cols":"50"
+            },
+            {
+                "label":"תגיות",
+                "tag":"input",
+                "type":"text"
+            },
+            {
+                "label":"קטגוריה",
+                "tag" :"select",
+                "options":[
+                    "1",
+                    "2",
+                    "3",
+                    "4"
+                ]
+            }
+
+        
+
+        ]
+
+    }
+}
+    insert = collection_name.insert_one(item)
+    return 'Hello Story'
 
 
 
